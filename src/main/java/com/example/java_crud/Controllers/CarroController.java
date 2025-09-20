092c9f973087842a1340f3e15e928a087ba2f6e0
luannagarla <luagarla@gmail.com>
2025-09-20 15:11:54 -0300

    Ajustes crud de carros

--- src/main/java/com/example/java_crud/Controllers/CarroController.java
+++ src/main/java/com/example/java_crud/Controllers/CarroController.java
@@ -1,4 +1,7 @@
 package com.example.java_crud.Controllers;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.example.java_crud.Models.Carro;
 import com.example.java_crud.Services.CarroService;
@@ -16,35 +19,47 @@ public class CarroController {
         this.carroService = carroService;
     }
 
-    @GetMapping("/carro")
-    public String clientesIndex() {
-        return "carro/index";
-    }
-
     @GetMapping
     public String listarCarros(Model model) {
         model.addAttribute("carros", carroService.listarTodos());
-        return "carros/lista";
+        return "carro/index"; // vai alimentar a grid
     }
 
     @GetMapping("/novo")
     public String novoCarro(Model model) {
         model.addAttribute("carro", new Carro());
-        return "carros/form";
+        return "carro/form";
     }
 
     @PostMapping("/salvar")
-    public String salvarCarro(@ModelAttribute Carro carro) {
-        carroService.salvar(carro);
-        return "redirect:/carros";
+    public ResponseEntity<?> salvarCarro(@ModelAttribute Carro Carro) {
+        try {
+            carroService.salvar(Carro);
+            return ResponseEntity.ok("Carro salvo com sucesso"); // 200
+        } catch (DataIntegrityViolationException ex) {
+            String mensagem = "Placa já existe no sistema!";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagem); // 400
+        }
     }
 
-    @GetMapping("/editar/{id}")
-    public String editarCarro(@PathVariable Long id, Model model) {
-        var carro = carroService.buscarPorId(id)
+    @GetMapping("/editar/form/{id}")
+    public String openEditarCarro(@PathVariable Long id, Model model) {
+        var Carro = carroService.buscarPorId(id)
                 .orElseThrow(() -> new IllegalArgumentException("Carro inválido: " + id));
-        model.addAttribute("carro", carro);
-        return "carros/form";
+        model.addAttribute("carro", Carro);
+        return "carro/form"; // abre o mesmo form, mas preenchido
+    }
+
+    @PostMapping("/editar/{id}")
+    public ResponseEntity<?> editarCarro(@PathVariable Long id, @ModelAttribute Carro Carro, Model model) {
+        Carro.setId(id);
+        try {
+            carroService.salvar(Carro);
+            return ResponseEntity.ok("Carro salvo com sucesso");
+        } catch (DataIntegrityViolationException ex) {
+            String mensagem = "Placa já existe no sistema!";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagem); // 400
+        }
     }
 
     @GetMapping("/excluir/{id}")
